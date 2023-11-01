c57f9b46f577d1134fff142286ec750737b0b238
Miguel <miguel.psilva5@senacsp.edu.br>
2023-11-01 17:08:45 -0300

    01/11/2023

--- includes/cabecalho.php
+++ includes/cabecalho.php
@@ -1,39 +1,28 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="pt-br">
 
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <title>Sistema Biblioteca</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
-    <link rel="stylesheet" type="text/css" href="/assets/css/stylecabecalho.css">
-    <style>
-        :root {
-            --bs-primary-rgb: 167, 126, 220;
-        }
-    </style>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/assets/css/Banner1.css">
+    <link rel="stylesheet" href="/assets/css/cabecalho.css">
+    <title>PMT</title>
 </head>
 
-<body class="d-flex flex-column min-vh-100">
-    </div>
-    <header>
-        <nav class="navbar navbar-expand-lg" style="background-color: rgb(var(--bs-primary-rgb));" data-bs-theme="light">
-            <div class="container">
-                <a class="navbar-brand custom-title" href="#" style="color: rgb(var(--bs-primary-rgb));">
-                    <img src="/assets/img/Logo PMT true.png" alt="Logo" height="50"> PMT Enterprise
-                </a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-                    <div class="navbar-nav">
-                        <a class="custom-button" href="/index.php">Home</a>
-                        <a class="custom-button" href="/admin/usuarios/">Fazer Login</a>
-                        <a class="custom-button" href="/admin/usuarios/cadastrar.php">Cadastrar-se</a>
-                        <a class="custom-button" href="#">Help</a>
-                    </div>
-                </div>
-            </div>
-        </nav>
+<body>
+
+    <header id="cabecalho" class="container">
+        <div class="logo">
+            <h1><img src="/assets/img/Logo PMT true.png">
+                <ul id="menu">
+                    <li><a href="#">Empresas</a></li>
+                    <li><a href="#">Informações</a></li>
+                    <li><a href="#">Sobre</a></li>
+                    <li><a href="#">Contato</a></li>
+                </ul>
+                <nav id="botaoCadastro">
+                    <button class="botao1">Cadastro</button>
+                    <button class="botao2">Login</button>
+                </nav>
+        </div>
     </header>
--- index.php
+++ index.php
@@ -1,37 +1,17 @@
-<?php
-require_once $_SERVER['DOCUMENT_ROOT'] . "/includes/cabecalho.php";
-require_once $_SERVER["DOCUMENT_ROOT"] ."/controllers/UsuarioController.php";
+<?php require_once $_SERVER['DOCUMENT_ROOT'] . '/includes/cabecalho.php'; ?>
+  <div>
+    <div class="banner1">
 
-$usuarioController = new UsuarioController();
-$usuarioController->listarUsuarios(); 
-?>
+      <h3 class="texto-encontre">Encontre,</h3>
 
-<!DOCTYPE html>
-<html lang="pt-br">
+      <h2 class="texto-conquiste">Conquiste,</h2>
 
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Banners</title>
-  <link rel="stylesheet" type="text/css" href="/assets/css/styleHome.css">
-  <link rel="stylesheet" type="text/css" href="/assets/css/styleHome2.css">
-</head>
+      <h1 class="texto-triunfe">Triunfe.</h1>
 
-<body>
-  <div class="banner bannerHome1">
-    <div class="content">
-      <h1>PMT</h1>
-      <p>Encontre, Conquiste, Triunfe</p>
     </div>
+
   </div>
 
-  <div class="banner bannerHome2">
-    <div class="content">
-      <h1 class="title-with-border">A Empresa</h1>
-      <p>Texto do segundo banner. Você pode adicionar qualquer conteúdo aqui.</p>
-    </div>
-  </div>
 </body>
 
 </html>
