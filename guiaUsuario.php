ad64354ee672215855179d22d31ebacdf73df909
Miguel <miguel.psilva5@senacsp.edu.br>
2023-11-13 13:48:42 -0300

    pega aí

--- guiaUsuario.php
+++ guiaUsuario.php
@@ -2,53 +2,77 @@
 
 <main>
     <button id="downloadButton">Baixar como PDF</button>
-    <h1 class="titulo-guia">O que é o PMT?</h1>
-    <img src="/assets/img/Logo PMT true.png">
-    <p class="texto-sobre">O PMT é o seu parceiro de confiança na jornada para conseguir o primeiro estágio. Desenvolvido com uma mistura de magia em PHP, JS, HTML e CSS, nosso software é sua passagem direta para oportunidades de estágio emocionantes.</p>
-
-    <h3>Como Funciona o PMT?</h3>
     <div>
-        <h4>Para Usuários temos esse guia</h4>
+        <h1 class="titulo-guia">O que é o PMT?</h1>
+        <img src="/assets/img/Logo PMT true.png">
+        <p class="texto-sobre">O PMT é o seu parceiro de confiança na jornada para conseguir o primeiro estágio.
+            Desenvolvido com uma mistura de magia em PHP, JS, HTML e CSS, nosso software é sua passagem direta para
+            oportunidades de estágio emocionantes.</p>
+
+        <h3>Como Funciona o PMT?</h3>
         <div>
-        <div>
-            <h4>Criando sua Conta</h4>
-            <img src=""></a>
-            <a href="/guiaUserCadastro.php">
-                <button type="button">Saiba Mais</button>
-            </a>
-        </div>
-           
-        <div>
-            <h4>Se tornando Candidato</h4>
-            <img src="">
-            <a href="/guiaCandidatoCadastro.php">
-                <button type="button">Saiba Mais</button>
-            </a>
+            <h4>Para Usuários temos esse guia</h4>
+            <div>
+                <h4>Criando sua Conta</h4>
+                <img src=""></a>
+                <a href="/guiaUserCadastro.php">
+                    <button type="button">Saiba Mais</button>
+                </a>
+            </div>
+
+            <div>
+                <h4>Se tornando Candidato</h4>
+                <img src="">
+                <a href="/guiaCandidatoCadastro.php">
+                    <button type="button">Saiba Mais</button>
+                </a>
+            </div>
+
+            <div>
+                <h4>Interaja com as Vagas</h4>
+                <img src="">
+                <a href="/guiaInteração.php">
+                    <button type="button">Saiba Mais</button>
+                </a>
+            </div>
+
+            <div>
+                <h4>Encontre, Conquiste, Triunfe</h4>
+                <img src="">
+                <a href="/guiaUserFinal.php">
+                    <button type="button">Saiba Mais</button>
+                </a>
+            </div>
         </div>
 
         <div>
-            <h4>Interaja com as Vagas</h4>
-            <img src="">
-            <a href="/guiaInteração.php">
-                <button type="button">Saiba Mais</button>
-            </a>
-
-        </div>
-            <h4>Encontre, Conquiste, Triunfe</h4>
-            <img src="">
-            <a href="/guiaUserFinal.php">
-                <button type="button">Saiba Mais</button>
-            </a>
-        </div>
-    </div>
-
-        <div>
-        <h4>E para as empresas temos esse guia</h4>
-        <div>
-            <a href="/guiaEmpresaCadastro.php"><img src=""></a>
-            <a href="/guiaVagaCadastro.php"><img src=""></a>
-            <a href="/guiaVerCandidatos.php"><img src=""></a>
-            <a href="/guiaAprovarRejeitar.php"><img src=""></a>
+            <h4>E para as empresas temos esse guia</h4>
+            <div>
+                <div>
+                    <h4>Criando Sua Conta</h4>
+                    <a href="/guiaEmpresaCadastro.php"><img src="">
+                        <button type="button">Saiba Mais</button>
+                    </a>
+                </div>
+                <div>
+                    <h4>Criando suas Vagas</h4>
+                    <a href="/guiaVagaCadastro.php"><img src="">
+                        <button type="button">Saiba Mais</button>
+                    </a>
+                </div>
+                <div>
+                    <h4>Candidatos Interessados</h4>
+                    <a href="/guiaVerCandidatos.php"><img src="">
+                        <button type="button">Saiba Mais</button>
+                    </a>
+                </div>
+                <div>
+                    <h4>Aprovar ou Rejeitar</h4>
+                    <a href="/guiaAprovarRejeitar.php"><img src="">
+                        <button type="button">Saiba Mais</button>
+                    </a>
+                </div>
+            </div>
         </div>
     </div>
 </main>
