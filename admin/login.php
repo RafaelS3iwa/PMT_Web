2b20d0281bfd29221434a8c364f39c583829fb2d
AaraoAbbate <134648305+AaraoAbbate@users.noreply.github.com>
2023-11-14 17:15:23 -0300

    atttatat

--- admin/login.php
+++ admin/login.php
@@ -3,21 +3,22 @@
 ?>
 
 <div id="escolhaLogin">
-    <form action="login.php" method="post">
-        <input type="radio" name="formType" value="usuario" id="usuario" checked>
-        <label for="usuario">Cadastro de Usuário</label>
+    <div id="botao-escolha-login">
+        <form action="login.php" method="post">
+            <input type="radio" name="formType" value="usuario" id="usuario" checked>
+            <label for="usuario">Cadastro de Usuário</label>
 
-        <input type="radio" name="formType" value="empresa" id="empresa">
-        <label for="empresa">Cadastro de Empresa</label>
-    </form>
-</div>
-
-<div id="usuarioForm">
-    <?php require_once $_SERVER['DOCUMENT_ROOT'] . "/admin/usuarios/login.php";?>
-</div>
-
-<div id="empresaForm" style="display:none">
-    <?php require_once $_SERVER['DOCUMENT_ROOT'] . "/admin/empresas/login.php";?>
+            <input type="radio" name="formType" value="empresa" id="empresa">
+            <label for="empresa">Cadastro de Empresa</label>
+        </form>
+    </div>
+    <div id="usuarioForm">
+        <?php require_once $_SERVER['DOCUMENT_ROOT'] . "/admin/usuarios/login.php";?>
+    </div>
+    
+    <div id="empresaForm" style="display:none">
+        <?php require_once $_SERVER['DOCUMENT_ROOT'] . "/admin/empresas/login.php";?>
+    </div>
 </div>
 
 <?php require_once $_SERVER['DOCUMENT_ROOT'] . "/includes/rodape.php"; ?>
