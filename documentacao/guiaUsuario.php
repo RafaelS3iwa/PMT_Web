199006f495b80dfa32a7fb6ba4457f18c29af75f
Miguel <miguel.psilva5@senacsp.edu.br>
2023-11-22 14:25:32 -0300

    update 2.0

--- documentacao/guiaUsuario.php
+++ documentacao/guiaUsuario.php
@@ -16,7 +16,7 @@
                 <div class="selecao-guia">
                     <div>
                         <h5>1. Criando sua Conta</h5>
-                        <img src="/assets/img/teste3.jpg" height="250"></a>
+                        <img src="/assets/img/pixel7.jpg" height="250"></a>
                         <a href="/documentacao/guiaUserCadastro.php">
                             <button type="button">Saiba Mais</button>
                         </a>
@@ -24,7 +24,7 @@
 
                     <div>
                         <h5>2. Se tornando Candidato</h5>
-                        <img src="/assets/img/teste3.jpg" height="250">
+                        <img src="/assets/img/pixel8.jpg" height="250">
                         <a href="/documentacao/guiaCandidatoCadastro.php">
                             <button type="button">Saiba Mais</button>
                         </a>
@@ -32,7 +32,7 @@
 
                     <div>
                         <h5>3. Interaja com as Vagas</h5>
-                        <img src="/assets/img/teste3.jpg" height="250">
+                        <img src="/assets/img/pixel9.jpg" height="250">
                         <a href="/documentacao/guiaInteracao.php">
                             <button type="button">Saiba Mais</button>
                         </a>
@@ -40,7 +40,7 @@
 
                     <div>
                         <h5>4. Encontre, Conquiste, Triunfe</h5>
-                        <img src="/assets/img/teste3.jpg" height="250">
+                        <img src="/assets/img/pixel3.jpg" height="250">
                         <a href="/documentacao/guiaUserFinal.php">
                             <button type="button">Saiba Mais</button>
                         </a>
@@ -51,28 +51,28 @@
                 <div class="selecao-guia">
                     <div>
                         <h5>1. Criando Sua Conta</h5>
-                        <img src="/assets/img/teste2.jpg" height="250">
+                        <img src="/assets/img/pixel17.jpg" height="250">
                         <a href="/documentacao/guiaEmpresaCadastro.php">
                             <button type="button">Saiba Mais</button>
                         </a>
                     </div>
                     <div>
                         <h5>2. Criando suas Vagas</h5>
-                        <img src="/assets/img/teste2.jpg" height="250">
+                        <img src="/assets/img/pixel13.jpg" height="250">
                         <a href="/documentacao/guiaVagaCadastro.php">
                             <button type="button">Saiba Mais</button>
                         </a>
                     </div>
                     <div>
                         <h5>3. Candidatos Interessados</h5>
-                        <img src="/assets/img/teste2.jpg" height="250">
+                        <img src="/assets/img/pixel14.jpg" height="250">
                         <a href="/documentacao/guiaVerCandidatos.php">
                             <button type="button">Saiba Mais</button>
                         </a>
                     </div>
                     <div>
                         <h5>4. Aprovar ou Rejeitar</h5>
-                        <img src="/assets/img/teste2.jpg" height="250">
+                        <img src="/assets/img/pixel15.jpg" height="250">
                         <a href="/documentacao/guiaAprovarRejeitar.php">
                             <button type="button">Saiba Mais</button>
                         </a>
